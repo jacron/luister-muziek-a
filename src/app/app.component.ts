a09b182fae6acfa4fa547fa839305e94c626ee4b
jhsm croonen <info@jcroonen.nl>
2019-01-01 18:39:01 +0100

    dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,56 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {MusicService} from './services/music.service';
 import {StateService} from './services/state.service';
-// import {OverlayContainer} from '@angular/cdk/overlay';
+import {OverlayContainer} from '@angular/cdk/overlay';
+
+const links = [
+  {
+    href: 'home',
+    label: 'Home',
+    icon: 'search',
+    color: 'orange',
+  },
+  {
+    href: 'search',
+    label: 'Zoek',
+    icon: 'search'
+  },
+  {
+    href: 'recent',
+    label: 'Recent',
+    icon: 'access_time'
+  },
+  {
+    href: 'pop',
+    label: 'Pop',
+    icon: 'person'
+  },
+  {
+    href: 'composer',
+    label: 'Componisten',
+    icon: 'person'
+  },
+  {
+    href: 'performer',
+    label: 'Performers',
+    icon: 'person'
+  },
+  {
+    href: 'collection',
+    label: 'Collecties',
+    icon: 'all_inbox'
+  },
+  {
+    href: 'tag',
+    label: 'Tags',
+    icon: 'bookmark'
+  },
+  {
+    href: 'code',
+    label: 'Codes',
+    icon: 'code'
+  },
+];
 
 @Component({
   selector: 'app-root',
@@ -10,64 +59,17 @@ import {StateService} from './services/state.service';
 })
 export class AppComponent implements OnInit {
 
-  // @ViewChild('maindiv') maindiv: ElementRef;
+  @ViewChild('maindiv') maindiv: ElementRef;
 
   appTitle = 'music-client';
   title = '';
-
-  links = [
-    {
-      href: 'home',
-      label: 'Home',
-      icon: 'search',
-      color: 'orange',
-    },
-    {
-      href: 'search',
-      label: 'Zoek',
-      icon: 'search'
-    },
-    {
-      href: 'recent',
-      label: 'Recent',
-      icon: 'access_time'
-    },
-    {
-      href: 'pop',
-      label: 'Pop',
-      icon: 'person'
-    },
-    {
-      href: 'composer',
-      label: 'Componisten',
-      icon: 'person'
-    },
-    {
-      href: 'performer',
-      label: 'Performers',
-      icon: 'person'
-    },
-    {
-      href: 'collection',
-      label: 'Collecties',
-      icon: 'all_inbox'
-    },
-    {
-      href: 'tag',
-      label: 'Tags',
-      icon: 'bookmark'
-    },
-    {
-      href: 'code',
-      label: 'Codes',
-      icon: 'code'
-    },
-  ];
+  links;
+  alternated = false;
 
   constructor(
     private musicService: MusicService,
     private stateService: StateService,
-    // private overlayContainer: OverlayContainer
+    private overlayContainer: OverlayContainer
   ) {
   }
 
@@ -75,13 +77,23 @@ export class AppComponent implements OnInit {
     this.musicService.controlPlayer(cmd).subscribe();
   }
 
-  // toggleTheme() {
-  //   const altTheme = 'alternate-theme';
-  //   // this.overlayContainer.getContainerElement().classList.add(altTheme);
-  //   this.maindiv.nativeElement.className = altTheme;
-  // }
+  toggleTheme() {
+    const altTheme = 'alternate-theme';
+    if (this.alternated) {
+      this.alternated = false;
+      this.overlayContainer.getContainerElement().classList.remove(altTheme);
+      this.maindiv.nativeElement.className = '';
+      // document.querySelector('body').className = '';
+    } else {
+      this.alternated = true;
+      this.overlayContainer.getContainerElement().classList.add(altTheme);
+      this.maindiv.nativeElement.className = altTheme;
+      // document.querySelector('body').className = altTheme;
+    }
+  }
 
   ngOnInit() {
+    this.links = links;
     this.stateService.currentTitle.subscribe(title => this.title = title);
   }
 }
