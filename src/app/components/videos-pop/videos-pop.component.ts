009796ede0fd3855beeb0e8d4cd9467a2b27296b
jhsm croonen <info@jcroonen.nl>
2019-01-13 22:52:03 +0100

    videoalbums pop

--- src/app/components/videos-pop/videos-pop.component.ts
+++ src/app/components/videos-pop/videos-pop.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MusicService} from '../../services/music.service';
+import {StateService} from '../../services/state.service';
 
 @Component({
   selector: 'app-videos-pop',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./videos-pop.component.scss']
 })
 export class VideosPopComponent implements OnInit {
+  videoalbums;
 
-  constructor() { }
+  constructor(
+    private musicService: MusicService,
+    private stateService: StateService,
+  ) { }
+
+  afterGetAlbums(results) {
+    console.log(results);
+    this.videoalbums = results;
+    this.stateService.setTitle('Muziekvideo\'s Pop');
+  }
+
+  getAlbums() {
+    this.musicService.getVideoAlbums('pop').subscribe(
+      results => this.afterGetAlbums(results)
+    )
+  }
 
   ngOnInit() {
+    this.getAlbums();
   }
 
 }
